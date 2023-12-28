eeea81350f446dc1af509382bbb2c4ea5afe1170
Thivi <theviyanthan@outlook.com>
2023-12-28 20:55:06 +0000

    feat(shared): add Forwarding Graph models

---
+++ shared/models/forwarding_graph.py
@@ -0,0 +1,47 @@
+"""
+Defines the `ForwardingGraphs` dictionary type and its associated types.
+"""
+
+from typing import Optional, TypedDict
+
+
+class ChainEntity(TypedDict):
+    """
+    Defines the `ChainEntity` dictionary type.
+    """
+
+    id: str
+    ip: Optional[str]
+
+
+class ForwardingLink:
+    """
+    Defines the `ForwardingLink` dictionary type.
+    """
+
+    source: ChainEntity
+    destination: ChainEntity
+
+
+class VNF(TypedDict):
+    """
+    Defines the `VNF` dictionary type.
+    """
+
+    node: ChainEntity
+    vnf: ChainEntity
+    next: "VNF | list[VNF] | str | list[str]"
+    isTraversed: Optional[bool]
+
+
+class ForwardingGraph(TypedDict):
+    """
+    Defines the `ForwardingGraph` dictionary type.
+    """
+
+    sfcID: str
+    vnfs: VNF
+    links: "list[ForwardingLink]"
+
+# pylint: disable=invalid-name
+ForwardingGraphs = "list[ForwardingGraph]"
