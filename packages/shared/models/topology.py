60d3acb61ba5da52d3bbd571ec9de0aa3577dc72
Thivi <theviyanthan@outlook.com>
2023-12-30 21:51:05 +0000

    feat(shared): add topology model

---
+++ packages/shared/models/topology.py
@@ -0,0 +1,50 @@
+"""
+Defines the `Topology` dictionary type and its associated types.
+"""
+
+from typing import Optional, TypedDict
+
+
+class Host(TypedDict):
+    """
+    Defines the `Host` dictionary type.
+    """
+
+    id: str
+    ip: Optional["list[str]"]
+    cpu: int
+    """
+    This is the number of CPUs to use. This is equal to CPU Quota / CPU Period.
+    The default CPU Period is 100000. So, in a 4-core machine, a CPU Quota of 200000
+    would mean that the host will use 2 cores. You can simply define the number of cores/CPUs to use
+    using this `cpu` attribute and the right CPU Quota will be calculated automatically.
+    """
+    memory: str
+
+
+class Switch(TypedDict):
+    """
+    Defines the `Switch` dictionary type.
+    """
+
+    id: str
+
+
+class Link(TypedDict):
+    """
+    Defines the `Link` dictionary type.
+    """
+
+    source: str
+    destination: str
+    bandwidth: int
+
+
+class Topology(TypedDict):
+    """
+    Defines the `Topology` dictionary type.
+    """
+
+    hosts: "list[Host]"
+    switches: "list[Switch]"
+    links: "list[Link]"
