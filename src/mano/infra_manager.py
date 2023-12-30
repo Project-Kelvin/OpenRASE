0ec11fbb5d43c181ba3f8e86243e0f624ceaf9f7
Thivi <theviyanthan@outlook.com>
2023-12-30 22:56:05 +0000

    feat(mano): add logic to add gateways to hosts in InfraManager

--- src/mano/infra_manager.py
+++ src/mano/infra_manager.py
@@ -37,12 +37,12 @@ class InfraManager():
         self.net.addController(self.ryu)
         self.net.start()
 
-    def generateIP(self) -> "Tuple[IPv4Address, IPv4Address]":
+    def generateIP(self) -> "Tuple[IPv4Network, IPv4Address, IPv4Address]":
         """
-        Generates an IP address for the network.
+        Generate an IP address for the network.
 
         Returns:
-        str: The generated IP address and the mask.
+        "Tuple[IPv4Network, IPv4Address, IPv4Address]": The generated IP address and the first two host IPs.
         """
 
         config: Config = getConfig()
@@ -53,11 +53,11 @@ class InfraManager():
         ip1: IPv4Address = next(ip.hosts())
         ip2: IPv4Address = next(ip.hosts())
 
-        return (ip1, ip2)
+        return (ip, ip1, ip2)
 
     def installTopology(self, topology: Topology) -> None:
         """
-        Spins up the provided topology virtually using Mininet (Containernet).
+        Spin up the provided topology virtually using Mininet (Containernet).
 
         Parameters:
         topology (Topology): The topology to be spun up.
@@ -68,14 +68,17 @@ class InfraManager():
 
         for host in topology['hosts']:
             ip: "Tuple[IPv4Address, IPv4Address]" = self.generateIP()
-            self.hostGateways[host['id']] = ip[0]
-            self.hosts.append(self.net.addHost(
+            self.hostGateways[host['id']] = ip[1]
+            host: Host = self.net.addHost(
                 host['id'],
-                ip=ip[1],
+                ip=ip[2],
                 cpu_quota=host['cpu'] * cpuPeriod,
                 mem_limit=host['memory'],
                 memswap_limit=host['memory']
-            ))
+            )
+            self.hosts.append(host)
+            host.cmd(f"ip route add default via {ip[1]}")
+
 
         for switch in topology['switches']:
             self.switches.append(self.net.addSwitch(
@@ -87,7 +90,7 @@ class InfraManager():
 
     def getNode(self, name: str) -> Host:
         """
-        Returns the node with the given name in the topology.
+        Get the node with the given name in the topology.
 
         Parameters:
         name (str): The name of the node to be returned.
@@ -100,21 +103,21 @@ class InfraManager():
 
     def getSwitches(self) -> Any:
         """
-        Returns the switches in the topology.
+        Get the switches in the topology.
         """
 
         return self.switches
 
     def getHosts(self) -> Any:
         """
-        Returns the hosts in the topology.
+        Get the hosts in the topology.
         """
 
         return self.hosts
 
     def assignIPs(self, fg: ForwardingGraph) -> ForwardingGraph:
         """
-        Assigns IPs to the hosts in the topology.
+        Assign IPs to the hosts in the topology.
 
         Parameters:
         fg (ForwardingGraph): The forwarding graph to be used to assign IPs.
@@ -124,23 +127,33 @@ class InfraManager():
         """
 
         vnfs: VNF = fg['vnfs']
+        vnfHosts: "TypedDict[str, Tuple[IPv4Address, IPv4Address]]" = {}
 
-        def traverseVNF(vnfs: VNF):
+        def traverseVNF(vnfs: VNF, vnfHosts: "TypedDict[str, Tuple[IPv4Address, IPv4Address]]"):
             """
-            Traverses the VNFs in the forwarding graph and assigns IPs to the hosts.
+            Traverse the VNFs in the forwarding graph and assigns IPs to the hosts.
+
+            Parameters:
+            vnfs (VNF): The VNF to be traversed.
+            vnfHosts (list[str]): The list of hosts in the VNF.
             """
 
             shouldContinue: bool = True
 
             while shouldContinue:
-                ipAddr: str = self.generateIP()
-                vnfs['host']['ip'] = ipAddr
+                ipAddr: "Tuple[IPv4Address, IPv4Address]" = self.generateIP()
+
+                vnfs['host']['ip'] = ipAddr[2]
+                vnfHosts[vnfs['host']['id']] = ipAddr
+
+                # Assign IP to the host
                 self.net.get(vnfs['host']['id']).cmd(
-                    f"ip addr add {ipAddr} dev {vnfs['host']['id']}-eth0")
+                    f"ip addr add {ipAddr[2]} dev {vnfs['host']['id']}-eth0")
+                # TODO: Assign ipAddr[1] to the switch
 
                 if isinstance(vnfs['next'], list):
                     for nextVnf in vnfs['next']:
-                        traverseVNF(nextVnf)
+                        traverseVNF(nextVnf, vnfHosts)
 
                     shouldContinue = False
                 else:
@@ -149,6 +162,13 @@ class InfraManager():
                 if vnfs == "terminal":
                     shouldContinue = False
 
-        traverseVNF(vnfs)
+        traverseVNF(vnfs, vnfHosts)
+
+        # Add gateways
+        for name, ips in vnfHosts.items():
+            host: Host = self.net.get(name)
+            for name1, ips2 in vnfHosts.items():
+                if name != name1:
+                    host.cmd(f"ip route add {str(ips2[0])} via {ips[1]}")
 
         return fg
