455ec0a9c23f87b9b0ce38cb35819ee05da491d8
Thivi <theviyanthan@outlook.com>
2024-01-02 22:16:19 +0000

    feat(mano): add traffic generator, server and SFCC to the topology

--- src/mano/infra_manager.py
+++ src/mano/infra_manager.py
@@ -16,6 +16,8 @@ from shared.models.topology import Topology
 from mininet.cli import CLI
 
 from sfc.sdn_controller import SDNController
+from constants.topology import SERVER, SFCC, SFCC_SWITCH, TRAFFIC_GENERATOR
+from constants.docker import DIND, SERVER_IMAGE
 
 class InfraManager():
     """
@@ -29,6 +31,7 @@ class InfraManager():
     hosts: "TypedDict[str, Host]" = {}
     switches: "TypedDict[str, OVSKernelSwitch]" = {}
     sdnController: SDNController = None
+    hostIPs: "TypedDict[str, Tuple[IPv4Network, IPv4Address, IPv4Address]]" = {}
 
     def __init__(self) -> None:
         """
@@ -71,21 +74,80 @@ class InfraManager():
         self.topology = topology
         cpuPeriod: int = 100000  # Docker default
         config: Config = getConfig()
-        hostGateways: "TypedDict[str, IPv4Address]" = {}
+
+        # Add traffic generator
+        ipTG: "Tuple[IPv4Network, IPv4Address, IPv4Address]" = self.generateIP()
+
+        tg: Host = self.net.addDocker(
+            TRAFFIC_GENERATOR,
+            ip=f"{ipTG[2]}/{ipTG[0].prefixlen}",
+            dimage=DIND,
+            privileged=True,
+            dcmd="dockerd",
+            volumes=[
+                config["repoAbsolutePath"]
+                + "/docker/compose:/home/docker"
+            ]
+        )
+
+        # Add SFCC Switch
+        sfccTGSwitch: "OVSKernelSwitch" = self.net.addSwitch(SFCC_SWITCH)
+        sfccTGSwitch.start([self.ryu])
+
+        # Add SFCC Switch-TG Link
+        self.net.addLink(tg, sfccTGSwitch)
+
+        # Add SFCC
+        ipSFCCTG: "Tuple[IPv4Network, IPv4Address, IPv4Address]" = self.generateIP()
+
+        sfcc: Host = self.net.addDocker(
+            SFCC,
+            ip=f"{ipSFCCTG[2]}/{ipSFCCTG[0].prefixlen}",
+            dimage=DIND,
+            privileged=True,
+            dcmd="dockerd",
+            volumes=[
+                config["repoAbsolutePath"]
+                + "/docker/compose:/home/docker"
+            ]
+        )
+        self.hosts[SFCC]=sfcc
+
+        # Add SFCC Link
+        self.net.addLink(sfccTGSwitch, sfcc)
+
+        # Add server
+        ipServer: "Tuple[IPv4Network, IPv4Address, IPv4Address]" = self.generateIP()
+        self.hostIPs[SERVER] = ipServer
+
+        server: Host = self.net.addDocker(
+            SERVER,
+            ip=f"{ipServer[2]}/{ipServer[0].prefixlen}",
+            dimage=SERVER_IMAGE,
+            privileged=True,
+            dcmd="poetry run python server.py"
+        )
+        self.hosts[SERVER]=server
 
         for host in topology['hosts']:
-            ip: "Tuple[IPv4Address, IPv4Address]" = self.generateIP()
-            hostGateways[host['id']] = ip[1]
+            ip: "Tuple[IPv4Network, IPv4Address, IPv4Address]" = self.generateIP()
+            self.hostIPs[host["id"]] = ip
 
-            hostNode: Host = self.net.addHost(
+            hostNode: Host = self.net.addDocker(
                 host['id'],
-                ip=f"{str(ip[2])}/{config['ipRange']['mask']}",
+                ip=f"{str(ip[2])}/{ip[0].prefixlen}",
                 cpu_quota=host['cpu'] * cpuPeriod,
                 mem_limit=host['memory'],
-                memswap_limit=host['memory']
+                memswap_limit=host['memory'],
+                dimage=DIND,
+                privileged=True,
+                dcmd="dockerd",
+                volumes=[
+                    config["repoAbsolutePath"]
+                    + "/docker/files:/home/docker"
+                ]
             )
             self.hosts[host["id"]]=hostNode
-            hostNode.cmd(f"ip route add default via {str(ip[1])}")
 
         for switch in topology['switches']:
             switchNode: "OVSKernelSwitch" = self.net.addSwitch(switch['id'])
@@ -94,12 +156,31 @@ class InfraManager():
 
         for link in topology['links']:
             self.net.addLink(
-                self.net.get(link['source']), self.net.get(link['destination']), bw=link['bandwidth'])
+                self.net.get(link['source']), self.net.get(link['destination']), bw=link['bandwidth'] if 'bandwidth' in link else None)
 
         self.net.start()
         sleep(5)
 
-        self.sdnController.assignSwitchIPs(topology, self.switches, hostGateways)
+        # Add ip to SFCC's interface connecting to the switch that connects to the rest of teh topology.
+        ipSFCCTopo: "Tuple[IPv4Network, IPv4Address, IPv4Address]" = self.generateIP()
+        sfcc.setIP(str(ipSFCCTopo[2]), prefixLen=ipSFCCTopo[0].prefixlen, intf=f"{sfcc.name}-eth1")
+        self.hostIPs[SFCC] = ipSFCCTopo
+
+        # Add routes
+        sfcc.cmd(f"ip route add {str(ipTG[0])} via {str(ipSFCCTG[1])}")
+        tg.cmd(f"ip route add {str(ipSFCCTG[0])} via {str(ipTG[1])}")
+
+        for host in self.hosts:
+            for host1 in self.hosts:
+                if self.hosts[host].name != self.hosts[host1].name:
+                    self.hosts[host].cmd(
+                        f"ip route add {str(self.hostIPs[host1][0])} via {str(self.hostIPs[host][1])}")
+
+        # Add SFCC switch ip addresses
+        self.sdnController.assignIP(ipSFCCTG[1], sfccTGSwitch)
+        self.sdnController.assignIP(ipTG[1], sfccTGSwitch)
+
+        self.sdnController.assignSwitchIPs(topology, self.switches, self.hostIPs)
 
 
     def getNode(self, name: str) -> Host:
@@ -141,7 +222,9 @@ class InfraManager():
         """
 
         vnfs: VNF = fg['vnfs']
-        vnfHosts: "TypedDict[str, Tuple[IPv4Address, IPv4Address]]" = {}
+        vnfHosts: "TypedDict[str, Tuple[IPv4Network, IPv4Address, IPv4Address]]" = {
+            SFCC: self.hostIPs[SFCC]
+        }
 
         def traverseVNF(vnfs: VNF, vnfHosts: "TypedDict[str, Tuple[IPv4Network, IPv4Address, IPv4Address]]"):
             """
@@ -182,14 +265,15 @@ class InfraManager():
 
         traverseVNF(vnfs, vnfHosts)
 
-        # Add gateways
+        # Add routes
         for name, ips in vnfHosts.items():
             host: Host = self.net.get(name)
             for name1, ips1 in vnfHosts.items():
                 if name != name1:
                     host.cmd(f"ip route add {str(ips1[0])} via {ips[1]}")
 
-        self.sdnController.installFlows(fg, vnfHosts, self.switches)
+        fg = self.sdnController.installFlows(fg, vnfHosts, self.switches)
+
         CLI(self.net)
         self.net.stop()
 
