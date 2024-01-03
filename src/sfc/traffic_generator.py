539120dc98a42c8dad38f3c25030b0ff1e37afe9
Thivi <theviyanthan@outlook.com>
2024-01-03 22:53:06 +0000

    feat(sfc): create TrafficGenerator

---
+++ src/sfc/traffic_generator.py
@@ -0,0 +1,25 @@
+"""
+Defines the TrafficGenerator class.
+"""
+
+class TrafficGenerator():
+    """
+    Class that generates traffic.
+    """
+
+    def setDesign(self, design) -> None:
+        """
+        Set the design of the traffic generator.
+
+        Parameters:
+            design (dict): The design of the traffic generator.
+        """
+
+        pass
+
+    def generateTraffic(self) -> None:
+        """
+        Generate traffic.
+        """
+
+        pass
