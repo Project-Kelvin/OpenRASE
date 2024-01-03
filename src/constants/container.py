760bc1a41de3b368c3c47a2fdc813faee3a62013
Thivi <theviyanthan@outlook.com>
2024-01-03 19:03:11 +0000

    feat(constants): add DinD, SFCC, SFF and Mininet constants

--- src/constants/container.py
+++ src/constants/container.py
@@ -7,4 +7,15 @@ from shared.utils.container import getRegistryContainerTag
 
 TAG = getRegistryContainerTag()
 SERVER_IMAGE = f"{TAG}/server:latest"
-DIND = f"{TAG}/dind:latest"
+DIND_IMAGE = f"{TAG}/dind:latest"
+DIND_TCP_PORT = 2375
+DIND_NETWORK1 = "dind_network1"
+DIND_NETWORK2 = "dind_network2"
+DIND_NW1_IP = "192.168.0.0/24"
+DIND_NW2_IP = "172.16.0.0/24"
+SFCC_IMAGE = f"{TAG}/sfcc:latest"
+MININET_PREFIX = "mn"
+SFF_IMAGE = f"{TAG}/sff:latest"
+SFF = "sff"
+SFF_IP1 = "192.168.0.2"
+SFF_IP2 = "172.16.0.2"
