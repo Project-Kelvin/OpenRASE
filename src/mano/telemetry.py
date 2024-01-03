c90358a18ac41a79cf2878886a6e963135581e81
Thivi <theviyanthan@outlook.com>
2024-01-03 22:57:39 +0000

    feat(mano): create Telemetry class

---
+++ src/mano/telemetry.py
@@ -0,0 +1,20 @@
+"""
+Defines the Telemetry class.
+"""
+
+class Telemetry():
+    """
+    Class that collects and sends telemetry data of the topology.
+    """
+
+    def getHostData(self) -> None:
+        """
+        Get the data of the hosts.
+        """
+        pass
+
+    def getSwitchData(self) -> None:
+        """
+        Get the data of the switches.
+        """
+        pass
