aa6278f4b352745d4910bd3c8589345a8211a664
Thivi <theviyanthan@outlook.com>
2024-01-05 00:03:24 +0000

    feat(utils): add a function to connect to DIND containers

---
+++ src/utils/container.py
@@ -0,0 +1,28 @@
+"""
+Defines Docker related utils.
+"""
+
+from docker import from_env, DockerClient
+from docker.models.containers import Container
+from constants.container import DIND_TCP_PORT, MININET_PREFIX
+
+def connectToDind(hostName: str) -> DockerClient:
+    """
+    Connect to DIND container and return teh Docker client.
+
+    Parameters:
+        hostName (str): The name of the host.
+
+    Returns:
+        DockerClient: The Docker client.
+    """
+
+    client: DockerClient = from_env()
+    hostContainer: Container = client.containers.get(
+        f"{MININET_PREFIX}.{hostName}")
+    hostIP: str = hostContainer.attrs["NetworkSettings"]["IPAddress"]
+
+    dindClient: DockerClient = DockerClient(
+        base_url=f"tcp://{hostIP}:{DIND_TCP_PORT}")
+
+    return dindClient
