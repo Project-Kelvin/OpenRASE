9df05abd0b7e5daff21fba219c5f0fe8305efbe4
Thivi <theviyanthan@outlook.com>
2024-01-05 00:02:52 +0000

    feat(utils): add a function to traverse VNF FGs

---
+++ src/utils/forwarding_graph.py
@@ -0,0 +1,42 @@
+"""
+Defines utils related to the forwarding graph.
+"""
+
+from typing import Callable
+from shared.models.forwarding_graph import VNF
+
+from constants.topology import TERMINAL
+
+
+def traverseVNF(vnfs: VNF, callback: Callable[[VNF], None], *args, shouldParseTerminal: bool = True) -> None:
+    """
+    Traverse the VNFs.
+
+    Parameters:
+        vnfs (VNF): The VNFs.
+        callback (Callable[[VNF], None]): The callback function.
+        *args: The arguments to be passed to the callback function.
+        shouldParseTerminal (bool): Whether to parse the terminal node or not.
+    """
+
+    def parseVNF(vnfs: VNF) -> None:
+        shouldContinue: bool = True
+
+        while shouldContinue:
+            if not shouldParseTerminal and vnfs["next"] == TERMINAL:
+                break
+
+            callback(vnfs, *args)
+
+            if isinstance(vnfs['next'], list):
+                for nextVnf in vnfs['next']:
+                    parseVNF(nextVnf)
+
+                shouldContinue = False
+            else:
+                vnfs = vnfs['next']
+
+            if vnfs == TERMINAL:
+                shouldContinue = False
+
+    parseVNF(vnfs)
