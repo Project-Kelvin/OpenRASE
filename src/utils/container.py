ef3ab03090705cc6fb7c3c6e0fcd5471dd86fecc
Thivi <theviyanthan@outlook.com>
2024-01-06 03:40:39 +0000

    feat(utils): extract out container IP getting logic into a separate function

--- src/utils/container.py
+++ src/utils/container.py
@@ -6,6 +6,25 @@ from docker import from_env, DockerClient
 from docker.models.containers import Container
 from constants.container import DIND_TCP_PORT, MININET_PREFIX
 
+
+def getContainerIP(hostName: str) -> str:
+    """
+    Get the IP address of a container.
+
+    Parameters:
+        hostName (str): The host name of the container.
+
+    Returns:
+        str: The IP address of the container.
+    """
+
+    client: DockerClient = from_env()
+    hostContainer: Container = client.containers.get(
+        f"{MININET_PREFIX}.{hostName}")
+    hostIP: str = hostContainer.attrs["NetworkSettings"]["IPAddress"]
+
+    return hostIP
+
 def connectToDind(hostName: str) -> DockerClient:
     """
     Connect to DIND container and return teh Docker client.
@@ -17,10 +36,7 @@ def connectToDind(hostName: str) -> DockerClient:
         DockerClient: The Docker client.
     """
 
-    client: DockerClient = from_env()
-    hostContainer: Container = client.containers.get(
-        f"{MININET_PREFIX}.{hostName}")
-    hostIP: str = hostContainer.attrs["NetworkSettings"]["IPAddress"]
+    hostIP: str = getContainerIP(hostName)
 
     dindClient: DockerClient = DockerClient(
         base_url=f"tcp://{hostIP}:{DIND_TCP_PORT}")
