f2de11cfd7dc4fa67ebc0572167f477b09f41bb3
Thivi <theviyanthan@outlook.com>
2024-01-06 03:42:13 +0000

    feat(sfc): subscribe TrafficGenerator to FG deployment notification

--- src/sfc/traffic_generator.py
+++ src/sfc/traffic_generator.py
@@ -2,10 +2,20 @@
 Defines the TrafficGenerator class.
 """
 
-class TrafficGenerator():
+from typing import Any
+
+from shared.models.forwarding_graph import ForwardingGraph
+from constants.notification import FORWARDING_GRAPH_DEPLOYED
+from mano.notification_system import Subscriber
+
+
+class TrafficGenerator(Subscriber):
     """
     Class that generates traffic.
     """
+
+    _design = None
+    _forwardingGraphs: "list[ForwardingGraph]" = []
 
     def setDesign(self, design) -> None:
         """
@@ -15,11 +25,25 @@ class TrafficGenerator():
             design (dict): The design of the traffic generator.
         """
 
-        pass
+        self._design = design
 
-    def generateTraffic(self) -> None:
+    def _generateTraffic(self) -> None:
         """
         Generate traffic.
         """
 
         pass
+
+    def getData(self) -> Any:
+        """
+        Get the data from the traffic generator.
+        """
+
+        pass
+
+    def receiveNotification(self, topic, *args: "list[Any]") -> None:
+        if topic == FORWARDING_GRAPH_DEPLOYED:
+            fg: ForwardingGraph = args[0]
+            self._forwardingGraphs.append(fg)
+
+            pass
