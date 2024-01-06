ec4c04daad24f4ecb108ceb5cfc48a88594d2013
Thivi <theviyanthan@outlook.com>
2024-01-06 03:43:31 +0000

    feat(sfc): subscribe SFCEmulator to SFF deployment notification

--- src/sfc/sfc_emulator.py
+++ src/sfc/sfc_emulator.py
@@ -2,15 +2,17 @@
 Defines the SFCEmulator class.
 """
 
-from typing import Type
+from typing import Any, Type
 from shared.models.topology import Topology
+from constants.notification import SFF_DEPLOYED
 from mano.mano_class import MANO
-from sfc.evolver import Evolver
+from mano.notification_system import NotificationSystem, Subscriber
+from sfc.solver import Solver
 from sfc.sfc_request_generator import SFCRequestGenerator
 from sfc.traffic_generator import TrafficGenerator
 
 
-class SFCEmulator():
+class SFCEmulator(Subscriber):
     """
     Class that emulates SFC and allows users to run experiments.
     """
@@ -18,32 +20,38 @@ class SFCEmulator():
     _mano: MANO = None
     _sfcRequestGenerator: SFCRequestGenerator = None
     _trafficGenerator: TrafficGenerator = None
+    _solver: Solver = None
 
-    def __init__(self) -> None:
+    def __init__(self, sfcRequestGenerator: Type[SFCRequestGenerator], solver: Type[Solver]) -> None:
         """
         Constructor for the class.
+
+        Parameters:
+            sfcRequestGenerator (SFCRequestGenerator): The SFC request generator.
+            solver (Type[Solver]): A child class of Solver.
         """
 
         self._mano = MANO()
-        self._sfcRequestGenerator = SFCRequestGenerator(self._mano.getOrchestrator())
         self._trafficGenerator = TrafficGenerator()
+        self._solver = solver(self._mano.getOrchestrator(), self._trafficGenerator)
+        self._sfcRequestGenerator = sfcRequestGenerator(self._solver)
+        NotificationSystem.subscribe(SFF_DEPLOYED, self)
 
-    def startTest(self, topology: Topology, trafficDesign, sfcRequestDesign, evolver: Type[Evolver]) -> None:
+
+    def startTest(self, topology: Topology, trafficDesign) -> None:
         """
         Start a test.
 
         Parameters:
             topology (Topology): The topology of the network.
-            trafficDesign (dict): The design of the traffic generator.
-            sfcRequestDesign (dict): The design of the SFC request generator.
-            evolver (Evolver): The evolver.
+            trafficDesign (dict): The design of the traffic generator..
         """
 
         self._mano.getInfraManager().installTopology(topology)
-        self._mano.getVNFManager().deploySFF()
-        self._sfcRequestGenerator.setDesign(sfcRequestDesign)
         self._trafficGenerator.setDesign(trafficDesign)
 
-        evolver(self._mano.getOrchestrator(), self._trafficGenerator)
 
-        self._sfcRequestGenerator.generateRequests()
+    def receiveNotification(self, topic, *args: "list[Any]") -> None:
+        if topic == SFF_DEPLOYED:
+            self._sfcRequestGenerator.generateRequests()
+            self._solver.generateForwardingGraphs()
