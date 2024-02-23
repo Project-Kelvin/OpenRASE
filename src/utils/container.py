c756285fbf1a95889b3dda496e6443e225efd85a
Thivi <theviyanthan@outlook.com>
2024-02-23 21:55:43 +0000

    feat(utils): allow connecting non mn dind containers

--- src/utils/container.py
+++ src/utils/container.py
@@ -7,12 +7,13 @@ from docker.models.containers import Container
 from constants.container import DIND_TCP_PORT, MININET_PREFIX
 
 
-def getContainerIP(hostName: str) -> str:
+def getContainerIP(hostName: str, isMnContainer: bool = True) -> str:
     """
     Get the IP address of a container.
 
     Parameters:
         hostName (str): The host name of the container.
+        isMnContainer (bool): Whether the container is a Mininet container.
 
     Returns:
         str: The IP address of the container.
@@ -20,23 +21,25 @@ def getContainerIP(hostName: str) -> str:
 
     client: DockerClient = from_env()
     hostContainer: Container = client.containers.get(
-        f"{MININET_PREFIX}.{hostName}")
+        f"{MININET_PREFIX}.{hostName}" if isMnContainer else hostName)
     hostIP: str = hostContainer.attrs["NetworkSettings"]["IPAddress"]
 
     return hostIP
 
-def connectToDind(hostName: str) -> DockerClient:
+
+def connectToDind(hostName: str, isMnContainer: bool = True) -> DockerClient:
     """
-    Connect to DIND container and return teh Docker client.
+    Connect to DIND container and return the Docker client.
 
     Parameters:
         hostName (str): The name of the host.
+        isMnContainer (bool): Whether the container is a Mininet container.
 
     Returns:
         DockerClient: The Docker client.
     """
 
-    hostIP: str = getContainerIP(hostName)
+    hostIP: str = getContainerIP(hostName, isMnContainer)
 
     dindClient: DockerClient = DockerClient(
         base_url=f"tcp://{hostIP}:{DIND_TCP_PORT}")
