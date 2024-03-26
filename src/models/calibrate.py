1fce0bc1232bcd588f4ca2b25de8cb930353025c
Thivi <theviyanthan@outlook.com>
2024-03-26 23:31:17 +0000

    feat(models): add calibrate model

---
+++ src/models/calibrate.py
@@ -0,0 +1,11 @@
+"""
+This defines teh models associated with calibration.
+"""
+
+class ResourceDemand(dict):
+    """
+    Defines the resource demand of a VNF.
+    """
+
+    cpu: float
+    memory: float
