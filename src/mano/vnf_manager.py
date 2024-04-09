edec04e41d6e0931b67db97798bd9d0fa2e3c03b
Thivi <theviyanthan@outlook.com>
2024-04-09 23:32:14 +0100

    fix(mano): add thread locks to fix issues caused by multi threading

--- src/mano/vnf_manager.py
+++ src/mano/vnf_manager.py
@@ -2,7 +2,7 @@
 Defines the VNFManager class that corresponds to the VNF Manager in the NFV architecture.
 """
 
-from threading import Thread
+from threading import Thread, Lock
 import requests
 from shared.models.embedding_graph import VNF, EmbeddingGraph, VNFEntity
 from shared.models.topology import Host as TopoHost
@@ -11,8 +11,8 @@ from shared.utils.container import getVNFContainerTag
 from constants.notification import EMBEDDING_GRAPH_DELETED, EMBEDDING_GRAPH_DEPLOYED
 from constants.topology import SERVER, SFCC
 from mano.infra_manager import InfraManager
-from docker import DockerClient
 from mano.notification_system import NotificationSystem
+from docker import DockerClient
 from utils.container import connectToDind, getContainerIP
 from utils.embedding_graph import traverseVNF
 
@@ -25,6 +25,8 @@ class VNFManager():
     _infraManager: InfraManager = None
     _embeddingGraphs: "dict[str, EmbeddingGraph]" = {}
     _ports: "dict[str, int]" = {}
+    _portLock = None
+    _infraLock = None
 
     def __init__(self, infraManager: InfraManager) -> None:
         """
@@ -35,6 +37,8 @@ class VNFManager():
         """
 
         self._infraManager = infraManager
+        self._portLock: Lock = Lock()
+        self._infraLock: Lock = Lock()
 
     def _deployEmbeddingGraph(self, eg: EmbeddingGraph) -> None:
         """
@@ -47,7 +51,8 @@ class VNFManager():
         if eg["sfcID"] in self._embeddingGraphs:
             self._deleteEmbeddingGraph(self._embeddingGraphs[eg["sfcID"]])
 
-        updatedEG: EmbeddingGraph = self._infraManager.embedSFC(eg)
+        with self._infraLock:
+            updatedEG: EmbeddingGraph = self._infraManager.embedSFC(eg)
         vnfs: VNF = updatedEG["vnfs"]
         sfcId: str = updatedEG["sfcID"]
         vnfList: "list[str]" = []
@@ -67,28 +72,32 @@ class VNFManager():
                 dindClient: DockerClient = connectToDind(f"{host['id']}Node")
 
                 volumes = {}
-                for vol in sharedVolumes[vnf["id"]]:
-                    volumes[vol.split(":")[0]] = {
-                        "bind": vol.split(":")[1],
-                        "mode": "rw"
-                    }
+                if vnf["id"] in sharedVolumes:
+                    for vol in sharedVolumes[vnf["id"]]:
+                        volumes[vol.split(":")[0]] = {
+                            "bind": vol.split(":")[1],
+                            "mode": "rw"
+                        }
 
-                if host["id"] in self._ports:
-                    self._ports[host["id"]] += 1
-                else:
-                    self._ports[host["id"]] = 5000
+                port: int = 5000
+                with self._portLock:
+                    if host["id"] in self._ports:
+                        self._ports[host["id"]] += 1
+                    else:
+                        self._ports[host["id"]] = 5000
+                    port = self._ports[host["id"]]
 
                 dindClient.containers.run(
                     getVNFContainerTag(vnf["id"]),
                     detach=True,
                     name=vnfName,
                     volumes=volumes,
-                    ports={"80/tcp": f"{self._ports[host['id']]}"},
+                    ports={"80/tcp": port},
                 )
 
-                vnf["ip"] = f"{getConfig()['sff']['network1']['hostIP']}:{self._ports[host['id']]}"
+                vnf["ip"] = f"{getConfig()['sff']['network1']['hostIP']}:{port}"
 
-        def traverseCallback(vnfs: VNF) -> None:
+        def traverseCallback(vnfs: VNF, _depth: int) -> None:
             """
             Callback function for the traverseVNF function.
 
@@ -115,6 +124,7 @@ class VNFManager():
             timeout=getConfig()["general"]["requestTimeout"]
         )
 
+        print(updatedEG)
         NotificationSystem.publish(EMBEDDING_GRAPH_DEPLOYED, updatedEG)
 
     def _deleteEmbeddingGraph(self, eg: EmbeddingGraph) -> None:
@@ -143,7 +153,7 @@ class VNFManager():
 
                 dindClient.containers.get(vnfName).remove(force=True)
 
-        def traverseCallback(vnfs: VNF) -> None:
+        def traverseCallback(vnfs: VNF, _depth: int) -> None:
             """
             Callback function for the traverseVNF function.
 
