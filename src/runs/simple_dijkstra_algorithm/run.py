cef0ebd78c13ea25af8739dc623fa2ca0de7beb7
Thivi <theviyanthan@outlook.com>
2024-04-29 03:25:19 +0100

    feat(runs): add dijkstra experiments

--- src/runs/simple_dijkstra_algorithm/run.py
+++ src/runs/simple_dijkstra_algorithm/run.py
@@ -4,6 +4,11 @@ This runs the Simple Dijsktra Algorithm.
 
 import copy
 import json
+from time import sleep
+from timeit import default_timer
+from typing import Union
+from models.traffic_generator import TrafficData
+from packages.shared.models.sfc_request import SFCRequest
 from shared.models.config import Config
 from shared.models.traffic_design import TrafficDesign
 from shared.utils.config import getConfig
@@ -18,13 +23,26 @@ from sfc.sfc_emulator import SFCEmulator
 from sfc.solver import Solver
 from sfc.traffic_generator import TrafficGenerator
 from utils.topology import generateFatTreeTopology
-from utils.traffic_design import generateTrafficDesign
-
+from utils.traffic_design import calculateTrafficDuration, generateTrafficDesign
+import click
+from utils.tui import TUI
 
 config: Config = getConfig()
 configPath: str = f"{config['repoAbsolutePath']}/src/runs/simple_dijkstra_algorithm/configs"
 
-topology: Topology = generateFatTreeTopology(4, 1000, 1, None)
+topology1: Topology = generateFatTreeTopology(4, 1000, 1, None)
+topologyPointFive: Topology = generateFatTreeTopology(4, 1000, 0.5, None)
+
+def appendToLog(message: str) -> None:
+    """
+    Append to the log.
+
+    Parameters:
+        message (str): The message to append.
+    """
+
+    with open(f"{config['repoAbsolutePath']}/src/runs/simple_dijkstra_algorithm/data/test.log", "a", encoding="utf8") as log:
+        log.write(f"{message}\n")
 
 with open(f"{configPath}/traffic-design.json", "r", encoding="utf8") as trafficFile:
     trafficDesign: "list[TrafficDesign]" = [json.load(trafficFile)]
@@ -34,7 +52,6 @@ class FGR(FGRequestGenerator):
     FG Request Generator.
     """
 
-
     def __init__(self, orchestrator: Orchestrator) -> None:
         super().__init__(orchestrator)
         self._fgs: "list[EmbeddingGraph]" = []
@@ -42,25 +59,87 @@ class FGR(FGRequestGenerator):
         with open(f"{configPath}/forwarding-graphs.json", "r", encoding="utf8") as fgFile:
             fgs: "list[EmbeddingGraph]" = json.load(fgFile)
             for fg in fgs:
-                for _i in range (2):
-                    self._fgs.append(copy.deepcopy(fg))
+                self._fgs.append(copy.deepcopy(fg))
 
+class FGR4SFC(FGR):
+    """
+    FG Request Generator that generates all 4 FGs once.
+    """
 
     def generateRequests(self) -> None:
         for index, fg in enumerate(self._fgs):
-            fg["sfcID"] = f"sfc{index}"
+            fg["sfcrID"] = f"sfc{index}"
 
         self._orchestrator.sendRequests(self._fgs)
+
+class FGR8SFC(FGR):
+    """
+    FG Request Generator that generates all 4 FGs twice.
+    """
+
+    def generateRequests(self) -> None:
+        copiedFGs: "list[EmbeddingGraph]" = []
+        for index, fg in enumerate(self._fgs):
+            fg["sfcrID"] = f"sfc{index}-1"
+            copiedFG: EmbeddingGraph = copy.deepcopy(fg)
+            copiedFG["sfcrID"] = f"sfc{index}-2"
+            copiedFGs.append(copiedFG)
+
+        self._fgs.extend(copiedFGs)
+
+        self._orchestrator.sendRequests(self._fgs)
+
+class FGR12SFC(FGR):
+    """
+    FG Request Generator that generates all 4 FGs thrice.
+    """
+
+    def generateRequests(self) -> None:
+        copiedFGs: "list[EmbeddingGraph]" = []
+        for index, fg in enumerate(self._fgs):
+            fg["sfcrID"] = f"sfc{index}-1"
+            copiedFG: EmbeddingGraph = copy.deepcopy(fg)
+            copiedFG1: EmbeddingGraph = copy.deepcopy(fg)
+            copiedFG["sfcrID"] = f"sfc{index}-2"
+            copiedFG1["sfcrID"] = f"sfc{index}-3"
+            copiedFGs.append(copiedFG)
+            copiedFGs.append(copiedFG1)
+
+        self._fgs.extend(copiedFGs)
+
+        self._orchestrator.sendRequests(self._fgs)
+
+class FGR16SFC(FGR):
+    """
+    FG Request Generator that generates all 4 FGs four times.
+    """
+
+    def generateRequests(self) -> None:
+        copiedFGs: "list[EmbeddingGraph]" = []
+        for index, fg in enumerate(self._fgs):
+            fg["sfcrID"] = f"sfc{index}-1"
+            copiedFG: EmbeddingGraph = copy.deepcopy(fg)
+            copiedFG1: EmbeddingGraph = copy.deepcopy(fg)
+            copiedFG2: EmbeddingGraph = copy.deepcopy(fg)
+            copiedFG["sfcrID"] = f"sfc{index}-2"
+            copiedFG1["sfcrID"] = f"sfc{index}-3"
+            copiedFG2["sfcrID"] = f"sfc{index}-4"
+            copiedFGs.append(copiedFG)
+            copiedFGs.append(copiedFG1)
+            copiedFGs.append(copiedFG2)
+
+        self._fgs.extend(copiedFGs)
+
+        self._orchestrator.sendRequests(self._fgs)
+
 
 class SFCSolver(Solver):
     """
     SFC Solver.
     """
 
-
     def __init__(self, orchestrator: Orchestrator, trafficGenerator: TrafficGenerator) -> None:
         super().__init__(orchestrator, trafficGenerator)
-        self._topology: Topology = None
         self._resourceDemands: "dict[str, ResourceDemand]" = None
 
         calibrate = Calibrate()
@@ -73,20 +152,174 @@ class SFCSolver(Solver):
         self._resourceDemands: "dict[str, ResourceDemand]" = calibrate.getResourceDemands(maxTarget)
 
     def generateEmbeddingGraphs(self) -> None:
-        sda = SimpleDijkstraAlgorithm(self._requests, topology, self._resourceDemands)
-        fgs, failedFGs, _nodes = sda.run()
-        print(f"Failed to deploy {len(failedFGs)} out of {len(fgs) + len(failedFGs)} FGs.")
-        self._orchestrator.sendEmbeddingGraphs(fgs)
+        try:
+            while self._requests.empty():
+                pass
+            requests: "list[Union[FGR, SFCRequest]]" = []
+            while not self._requests.empty():
+                requests.append(self._requests.get())
+            self._topology: Topology = self._orchestrator.getTopology()
+            sda = SimpleDijkstraAlgorithm(requests, self._topology, self._resourceDemands)
+            start: float = default_timer()
+            fgs, failedFGs, _nodes = sda.run()
+            end: float = default_timer()
+            executionTime = end - start
+            TUI.appendToSolverLog(f"Failed FGs: {len(failedFGs)}")
+            TUI.appendToSolverLog(f"Accepted FGs: {len(fgs)}")
+            appendToLog(f"Acceptance Ratio: {len(fgs) / (len(fgs) + len(failedFGs)) * 100:.2f}%")
+            TUI.appendToSolverLog(f"Acceptance Ratio: {len(fgs) / (len(fgs) + len(failedFGs)) * 100:.2f}%")
+            appendToLog(f"Execution Time: {executionTime:.6f}s")
+            TUI.appendToSolverLog(f"Execution Time: {executionTime:.6f}s")
 
-def run() -> None:
+            start = default_timer()
+            self._orchestrator.sendEmbeddingGraphs(fgs)
+            end = default_timer()
+
+            deploymentTime = end - start
+
+            appendToLog(f"Deployment Time: {deploymentTime:.6f}s")
+            TUI.appendToSolverLog(f"Deployment Time: {deploymentTime:.6f}s")
+
+            trafficDuration: int = calculateTrafficDuration(self._trafficGenerator.getDesign()[0])
+            TUI.appendToSolverLog(f"Waiting for {trafficDuration}s.")
+            sleep(trafficDuration)
+            TUI.appendToSolverLog(f"Finished waiting.")
+
+            data: "dict[str, TrafficData]" = self._trafficGenerator.getData(f"{trafficDuration}s")
+            appendToLog("Average latency in each SFC:")
+            TUI.appendToSolverLog("Average latency in each SFC:")
+            for key, value in data.items():
+                appendToLog(f"{key}: {value['averageLatency']:.2f}")
+                TUI.appendToSolverLog(f"{key}: {value['averageLatency']:.2f}")
+            TUI.appendToSolverLog(f"Finished experiment.")
+            sleep(2)
+        except Exception as e:
+            TUI.appendToSolverLog(str(e), True)
+        TUI.exit()
+
+@click.command()
+@click.option("--experiment", type=int, help="The experiment to run.")
+def run(experiment: int) -> None:
     """
     Run the Simple Dijkstra Algorithm.
+
+    Parameters:
+        experiment (int): The experiment to run.
     """
 
-    sfcEm: SFCEmulator = SFCEmulator(FGR, SFCSolver)
-    sfcEm.startTest(topology, trafficDesign)
-    sfcEm.startCLI()
-    sfcEm.end()
+    def runExperiment(fgr: FGR, topology: Topology) -> None:
+        """
+        Run an experiment.
+
+        Parameters:
+            fgr (FGR): The FG Request Generator.
+            topology (Topology): The topology.
+        """
+
+        sfcEm: SFCEmulator = SFCEmulator(fgr, SFCSolver)
+        sfcEm.startTest(topology, trafficDesign)
+        sfcEm.end()
+        appendToLog("--------------------------")
+
+    def experiment1() -> None:
+        """
+        Run Experiment 1.
+        """
+
+        # Experiment 1 - 4 SFCs 0.5
+        appendToLog("SFCs: 4, Topology: 0.5")
+        runExperiment(FGR4SFC, topologyPointFive)
+
+    def experiment2() -> None:
+        """
+        Run Experiment 2.
+        """
+
+        # Experiment 2 - 8 SFCs 0.5
+        appendToLog("SFCs: 8, Topology: 0.5")
+        runExperiment(FGR8SFC, topologyPointFive)
+
+    def experiment3() -> None:
+        """
+        Run Experiment 3.
+        """
+
+        # Experiment 3 - 12 SFCs 0.5
+        appendToLog("SFCs: 12, Topology: 0.5")
+        runExperiment(FGR12SFC, topologyPointFive)
+
+    def experiment4() -> None:
+        """
+        Run Experiment 4.
+        """
+
+        # Experiment 4 - 16 SFCs 0.5
+        appendToLog("SFCs: 16, Topology: 0.5")
+        runExperiment(FGR16SFC, topologyPointFive)
+
+    def experiment5() -> None:
+        """
+        Run Experiment 5.
+        """
+
+        # Experiment 5 - 4 SFCs 1
+        appendToLog("SFCs: 4, Topology: 1")
+        runExperiment(FGR4SFC, topology1)
+
+    def experiment6() -> None:
+        """
+        Run Experiment 6.
+        """
+
+        # Experiment 6 - 8 SFCs 1
+        appendToLog("SFCs: 8, Topology: 1")
+        runExperiment(FGR8SFC, topology1)
+
+    def experiment7() -> None:
+        """
+        Run Experiment 7.
+        """
+
+        # Experiment 7 - 12 SFCs 1
+        appendToLog("SFCs: 12, Topology: 1")
+        runExperiment(FGR12SFC, topology1)
+
+    def experiment8() -> None:
+        """
+        Run Experiment 8.
+        """
+
+        # Experiment 8 - 16 SFCs 1
+        appendToLog("SFCs: 16, Topology: 1")
+        runExperiment(FGR16SFC, topology1)
+
+    if experiment == 1:
+        experiment1()
+    elif experiment == 2:
+        experiment2()
+    elif experiment == 3:
+        experiment3()
+    elif experiment == 4:
+        experiment4()
+    elif experiment == 5:
+        experiment5()
+    elif experiment == 6:
+        experiment6()
+    elif experiment == 7:
+        experiment7()
+    elif experiment == 8:
+        experiment8()
+    else:
+        experiment1()
+        experiment2()
+        experiment3()
+        experiment4()
+        experiment5()
+        experiment6()
+        experiment7()
+        experiment8()
+
+
 
 def getTrafficDesign() -> None:
     """
@@ -94,7 +327,7 @@ def getTrafficDesign() -> None:
     """
 
     design: TrafficDesign = generateTrafficDesign(
-        f"{getConfig()['repoAbsolutePath']}/src/runs/simple_dijkstra_algorithm/data/requests.csv")
+        f"{getConfig()['repoAbsolutePath']}/src/runs/simple_dijkstra_algorithm/data/requests.csv", 2)
 
     with open(f"{configPath}/traffic-design.json", "w", encoding="utf8") as traffic:
         json.dump(design, traffic, indent=4)
