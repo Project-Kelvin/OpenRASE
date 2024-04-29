525f5bb2ed63c30a3dba49c4fff914ad0e32ec6c
Thivi <theviyanthan@outlook.com>
2024-04-29 03:24:59 +0100

    feat(utils): add a method to calculate duration base don traffic design

--- src/utils/traffic_design.py
+++ src/utils/traffic_design.py
@@ -5,12 +5,13 @@ This defines utils associated with traffic design.
 from shared.models.traffic_design import TrafficDesign
 
 
-def generateTrafficDesign(dataFile: str) -> "TrafficDesign":
+def generateTrafficDesign(dataFile: str, scale: int = 1) -> "TrafficDesign":
     """
     Generate the Traffic Design.
 
     Parameters:
         dataFile (str): the CSV data file.
+        scale (int): the scale factor.
 
     Returns:
         TrafficDesign: the Traffic Design.
@@ -22,8 +23,8 @@ def generateTrafficDesign(dataFile: str) -> "TrafficDesign":
         _header = file.readline()  # Read and discard the header
         for line in file:
             numOfReqs: int = int(line)
-            # scale by 20
-            numOfReqs = numOfReqs * 20
+            # scale by 1
+            numOfReqs = numOfReqs * scale
             #Considering an hour is equal to 4 seconds
             rate: int = numOfReqs // 4
             design.append({
@@ -32,3 +33,28 @@ def generateTrafficDesign(dataFile: str) -> "TrafficDesign":
             })
 
     return design
+
+def calculateTrafficDuration(trafficDesign: TrafficDesign) -> int:
+    """
+    Calculate the duration of the traffic.
+
+    Parameters:
+        trafficDesign (TrafficDesign): The traffic design.
+
+    Returns:
+        int: The duration of the traffic.
+    """
+
+    totalDuration: int = 0
+
+    for traffic in trafficDesign:
+        durationText: str = traffic["duration"]
+        unit: str  = durationText[-1]
+        if unit == "s":
+            totalDuration += int(durationText[:-1])
+        elif unit == "m":
+            totalDuration += int(durationText[:-1]) * 60
+        elif unit == "h":
+            totalDuration += int(durationText[:-1]) * 60 * 60
+
+    return totalDuration
