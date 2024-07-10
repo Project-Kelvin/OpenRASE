1c9722937293f0bc584d46ecd31975ede229c887
Thivi <theviyanthan@outlook.com>
2024-07-10 23:32:28 +0100

    feat(algorithms): record Pareto front for each generation

--- src/algorithms/ga_dijkstra_algorithm/ga.py
+++ src/algorithms/ga_dijkstra_algorithm/ga.py
@@ -24,6 +24,9 @@ NO_OF_INDIVIDUALS: int = 10
 with open(f"{getConfig()['repoAbsolutePath']}/artifacts/experiments/ga_dijkstra_algorithm/data.csv", "w", encoding="utf8") as topologyFile:
     topologyFile.write("generation, average_ar, max_ar, min_ar, average_latency, max_latency, min_latency\n")
 
+with open(f"{getConfig()['repoAbsolutePath']}/artifacts/experiments/ga_dijkstra_algorithm/pfs.csv", "w", encoding="utf8") as pf:
+    pf.write("generation, latency, ar\n")
+
 def GADijkstraAlgorithm(topology: Topology, resourceDemands: "dict[str, ResourceDemand]", fgrs: "list[EmbeddingGraph]", sendEGs: "Callable[[list[EmbeddingGraph]], None]", deleteEGs: "Callable[[list[EmbeddingGraph]], None]", trafficDesign: TrafficDesign, trafficGenerator: TrafficGenerator) -> "tuple[tools.ParetoFront]":
     """
     Run the Genetic Algorithm + Dijkstra Algorithm.
@@ -53,21 +56,36 @@ def GADijkstraAlgorithm(topology: Topology, resourceDemands: "dict[str, Resource
 
     pop = toolbox.population(n=NO_OF_INDIVIDUALS)
     gen: int = 1
-    CXPB, MUTPB, NGEN = 0.8, 0.2, 10
+    CXPB, MUTPB, NGEN = 0.8, 0.8, 10
     for ind in pop:
         ind.fitness.values = evaluation(ind, fgrs, gen, NGEN, sendEGs, deleteEGs, trafficDesign, trafficGenerator, topology, resourceDemands)
 
-    gen = gen + 1
+    ars = [ind.fitness.values[0] for ind in pop]
+    latencies = [ind.fitness.values[1] for ind in pop]
+
+    with open(f"{getConfig()['repoAbsolutePath']}/artifacts/experiments/ga_dijkstra_algorithm/data.csv", "a", encoding="utf8") as topologyFile:
+        topologyFile.write(f"{gen}, {np.mean(ars)}, {max(ars)}, {min(ars)}, {np.mean(latencies)}, {max(latencies)}, {min(latencies)}\n")
 
     hof = tools.ParetoFront()
+    hof.update(pop)
 
-    while gen < NGEN:
+    for ind in hof:
+        with open(f"{getConfig()['repoAbsolutePath']}/artifacts/experiments/ga_dijkstra_algorithm/pfs.csv", "a", encoding="utf8") as pf:
+            pf.write(f"{gen}, {ind.fitness.values[1]}, {ind.fitness.values[0]}\n")
+
+    gen = gen + 1
+
+    while gen <= NGEN:
         TUI.appendToSolverLog(f"Generation: {gen}")
         offspring = algorithm(pop, toolbox, CXPB, MUTPB, topology, resourceDemands, fgrs)
         for ind in offspring:
             ind.fitness.values = evaluation(ind, fgrs, gen, NGEN, sendEGs, deleteEGs, trafficDesign, trafficGenerator, topology, resourceDemands)
         pop = toolbox.select(pop + offspring, k=NO_OF_INDIVIDUALS)
         hof.update(pop)
+
+        for ind in hof:
+            with open(f"{getConfig()['repoAbsolutePath']}/artifacts/experiments/ga_dijkstra_algorithm/pfs.csv", "a", encoding="utf8") as pf:
+                pf.write(f"{gen}, {ind.fitness.values[1]}, {ind.fitness.valuse[0]}\n")
 
         ars = [ind.fitness.values[0] for ind in pop]
         latencies = [ind.fitness.values[1] for ind in pop]
