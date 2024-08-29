a8aeabec855c07f4804fbe18555748bf7f879aae
Thivi <theviyanthan@outlook.com>
2024-08-30 00:21:25 +0100

    feat(algorithms): add function to use NN as a genetic encoding scheme

--- src/algorithms/surrogacy/nn.py
+++ src/algorithms/surrogacy/nn.py
@@ -2,16 +2,14 @@
 This defines the Neural Network used for genetic encoding.
 """
 
-import json
-import random
 import pandas as pd
 from shared.models.embedding_graph import VNF, EmbeddingGraph
 from shared.models.topology import Topology
 from shared.utils.config import getConfig
 
 from utils.embedding_graph import traverseVNF
-from utils.topology import generateFatTreeTopology
-
+import tensorflow as tf
+import numpy as np
 
 def convertFGstoDF(fgs: "list[EmbeddingGraph]", topology: Topology) -> pd.DataFrame:
     """
@@ -20,6 +18,9 @@ def convertFGstoDF(fgs: "list[EmbeddingGraph]", topology: Topology) -> pd.DataFr
     Parameters:
         fgs (list[EmbeddingGraph]): the list of EmbeddingGraphs.
         topology (Topology): the topology.
+
+    Returns:
+        pd.DataFrame: the Pandas Dataframe.
     """
 
     vnfs: "list[str]" = getConfig()["vnfs"]["names"]
@@ -39,6 +40,7 @@ def convertFGstoDF(fgs: "list[EmbeddingGraph]", topology: Topology) -> pd.DataFr
             vnfID: int = vnfs.index(vnf["vnf"]["id"]) + 1
 
             for hIndex, _host in enumerate(topology["hosts"]):
+                #pylint: disable=cell-var-from-loop
                 data.append([fgID, vnfID, pos, hIndex + 1])
 
         traverseVNF(fg["vnfs"], parseVNF, shouldParseTerminal=False)
@@ -46,7 +48,7 @@ def convertFGstoDF(fgs: "list[EmbeddingGraph]", topology: Topology) -> pd.DataFr
     return pd.DataFrame(data, columns=["SFC", "VNF", "Position", "Host"])
 
 
-def generateEmbeddingGraphs(data: pd.DataFrame, fgs: "list[EmbeddingGraph]", topology: Topology) -> "list[EmbeddingGraph]":
+def convertDFtoFGs(data: pd.DataFrame, fgs: "list[EmbeddingGraph]", topology: Topology) -> "list[EmbeddingGraph]":
     """
     Generates the Embedding Graphs.
 
@@ -54,23 +56,26 @@ def generateEmbeddingGraphs(data: pd.DataFrame, fgs: "list[EmbeddingGraph]", top
         data (pd.DataFrame): the data.
         fgs (list[EmbeddingGraph]): the list of Embedding Graphs.
         topology (Topology): the topology.
+
+    Returns:
+        list[EmbeddingGraph]: the Embedding Graphs.
     """
 
     noHosts: int = len(topology["hosts"])
     startIndex: "list[int]" = [0]
-    endIndex: "list[int]" = [noHosts - 1]
+    endIndex: "list[int]" = [noHosts]
 
     egs: "list[EmbeddingGraph]" = []
-    for index, fg in enumerate(fgs):
+    for _index, fg in enumerate(fgs):
         embeddingNotFound: "list[bool]" = [False]
 
-        def parseVNF(vnf: VNF, pos: int, startIndex, endIndex) -> None:
+        def parseVNF(vnf: VNF, _pos: int, embeddingNotFound, startIndex, endIndex) -> None:
             """
             Parses a VNF.
 
             Parameters:
                 vnf (VNF): the VNF.
-                pos (int): the position.
+                _pos (int): the position.
                 startIndex (list[int]): the start index.
                 endIndex (list[int]): the end index.
             """
@@ -93,7 +98,7 @@ def generateEmbeddingGraphs(data: pd.DataFrame, fgs: "list[EmbeddingGraph]", top
                     "id": topology["hosts"][cls.index(maxCL)]["id"]
                 }
 
-        traverseVNF(fg["vnfs"], parseVNF, startIndex, endIndex, shouldParseTerminal=False)
+        traverseVNF(fg["vnfs"], parseVNF, embeddingNotFound, startIndex, endIndex, shouldParseTerminal=False)
 
         if not embeddingNotFound[0]:
             egs.append(fg)
@@ -101,16 +106,45 @@ def generateEmbeddingGraphs(data: pd.DataFrame, fgs: "list[EmbeddingGraph]", top
     return egs
 
 
-topo: Topology = generateFatTreeTopology(4, 1000, 2, 1000)
+def getConfidenceValues(data: pd.DataFrame, weights: "list[float]", bias: "list[float]") -> pd.DataFrame:
+    """
+    Gets the confidence values.
 
-with open(f"{getConfig()['repoAbsolutePath']}/src/runs/simple_dijkstra_algorithm/configs/forwarding-graphs.json", "r") as file:
-    fgs: "list[EmbeddingGraph]" = json.load(file)
+    Parameters:
+        data (pd.DataFrame): the data.
+        weights (list[list[float]]): the weights.
+        bias (list[float]): the bias.
 
-    data = convertFGstoDF(fgs, topo)
-    cls  =[]
-    for _i in range(len(data)):
-        cls.append(random.uniform(0, 1))
-    data = data.assign(ConfidenceLevel = cls)
-    egs = generateEmbeddingGraphs(data, fgs, topo)
+    Returns:
+        pd.DataFrame: the confidence values.
+    """
 
-    print(egs)
+    layers: "list[int]" = [4, 1]
+    copiedData = data.copy()
+    normalizer = tf.keras.layers.Normalization(axis=1)
+    normalizer.adapt(np.array(copiedData))
+
+    model = tf.keras.Sequential([
+        tf.keras.Input(shape=(layers[0], )),
+        normalizer,
+        tf.keras.layers.Dense(layers[1], activation='sigmoid')
+    ])
+
+    index: int = 0
+    startIndex: int = 0
+    endIndex: int = layers[index]
+    for layer in model.layers:
+        if isinstance(layer, tf.keras.layers.Dense):
+            layer.set_weights([
+                np.array(weights[startIndex:endIndex]).reshape(layers[index], layers[index + 1]),
+                np.array([bias[index]])
+            ])
+            index += 1
+            if index < len(layers) - 1:
+                startIndex = endIndex
+                endIndex = startIndex + layers[index]
+
+    prediction = model.predict(np.array(copiedData))
+    copiedData = copiedData.assign(ConfidenceLevel = prediction)
+
+    return copiedData
