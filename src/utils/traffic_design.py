6fff4ee695b1d4fadf768804b487a848b971e1a9
Thivi <theviyanthan@outlook.com>
2025-03-23 01:27:06 +0000

    feat(utils): modify generate traffic design function and add a new function to get the list of request rates in a design

--- src/utils/traffic_design.py
+++ src/utils/traffic_design.py
@@ -2,10 +2,11 @@
 This defines utils associated with traffic design.
 """
 
+import numpy as np
 from shared.models.traffic_design import TrafficDesign
 
 
-def generateTrafficDesign(dataFile: str, scale: float = 1.5, hourDuration: float = 4) -> "TrafficDesign":
+def generateTrafficDesign(dataFile: str, scale: float = 1, hourDuration: float = 4, minimal: bool = False) -> "TrafficDesign":
     """
     Generate the Traffic Design.
 
@@ -13,6 +14,7 @@ def generateTrafficDesign(dataFile: str, scale: float = 1.5, hourDuration: float
         dataFile (str): the CSV data file.
         scale (float): the scale factor.
         hourDuration (float): the simulated duration of the hour in seconds.
+        minimal (bool): whether to generate minimal traffic design.
 
     Returns:
         TrafficDesign: the Traffic Design.
@@ -22,16 +24,22 @@ def generateTrafficDesign(dataFile: str, scale: float = 1.5, hourDuration: float
 
     with open(dataFile, "r", encoding="utf8") as file:
         _header = file.readline()  # Read and discard the header
+        lastReqps: int = 0
         for line in file:
             numOfReqs: int = int(line)
-            # scale by 1
+            # scale
             numOfReqs = round(numOfReqs * scale)
+            if minimal and lastReqps == numOfReqs:
+                continue
+            lastReqps = numOfReqs
             #Considering an hour is equal to `hourDuration` seconds
             rate: int = numOfReqs // hourDuration
-            design.append({
-                "target": rate,
-                "duration": f'{hourDuration}s'
-            })
+
+            for _ in range(hourDuration):
+                design.append({
+                    "target": rate,
+                    "duration": "1s"
+                })
 
     return design
 
@@ -59,3 +67,26 @@ def calculateTrafficDuration(trafficDesign: TrafficDesign) -> int:
             totalDuration += int(durationText[:-1]) * 60 * 60
 
     return totalDuration
+
+def getTrafficDesignRate(trafficDesign: TrafficDesign, durations: "list[int]") -> "list[float]":
+    """
+    Get the traffic design rate.
+
+    Parameters:
+        trafficDesign (TrafficDesign): The traffic design.
+        durations (list[int]): The durations.
+
+    Returns:
+        list[float]: The traffic design rate.
+    """
+
+    rate: "list[float]" = [traffic["target"] for traffic in trafficDesign]
+    currRate: int = 1
+
+    reqps: "list[float]" = []
+
+    for duration in durations:
+        reqps.append(np.mean(rate[:duration]) if len(rate) != 0 else currRate)
+        rate = rate[duration:]
+
+    return reqps
