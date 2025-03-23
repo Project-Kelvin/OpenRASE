26fdcbc3dad430cd93f808ea47ad4567bbd5a2e0
Thivi <theviyanthan@outlook.com>
2025-03-23 02:37:18 +0000

    feat(algorithms): add surrogacy constants

--- src/algorithms/surrogacy/local_constants.py
+++ src/algorithms/surrogacy/local_constants.py
@@ -2,4 +2,11 @@
 Defines the constants used by the Surrogacy algorithm.
 """
 
+from shared.utils.config import getConfig
+
+
 BRANCH: str = "BRANCH"
+SURROGACY_PATH: str = f"{getConfig()['repoAbsolutePath']}/artifacts/surrogacy"
+SURROGATE_PATH: str = f"{SURROGACY_PATH}/surrogate"
+SURROGATE_DATA_PATH: str = f"{SURROGATE_PATH}/data"
+SURROGATE_MODELS_PATH: str = f"{SURROGATE_PATH}/models"
