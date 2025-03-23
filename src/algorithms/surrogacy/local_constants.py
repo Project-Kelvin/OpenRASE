06bdca47c8e1a7df84f0fb6657e2ccfd55c8ea22
Thivi <theviyanthan@outlook.com>
2025-03-23 16:17:27 +0000

    fix(algorithms): fix surrogacy artifact path

--- src/algorithms/surrogacy/local_constants.py
+++ src/algorithms/surrogacy/local_constants.py
@@ -6,7 +6,7 @@ from shared.utils.config import getConfig
 
 
 BRANCH: str = "BRANCH"
-SURROGACY_PATH: str = f"{getConfig()['repoAbsolutePath']}/artifacts/surrogacy"
+SURROGACY_PATH: str = f"{getConfig()['repoAbsolutePath']}/artifacts/experiments/surrogacy"
 SURROGATE_PATH: str = f"{SURROGACY_PATH}/surrogate"
 SURROGATE_DATA_PATH: str = f"{SURROGATE_PATH}/data"
 SURROGATE_MODELS_PATH: str = f"{SURROGATE_PATH}/models"
