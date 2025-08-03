fix(algorithms): fix link score calculation bug

src/algorithms/surrogacy/solvers/link_embedding.py
@@ -8,12 +8,13 @@ import networkx as nx
 import heapq
 from algorithms.surrogacy.constants.surrogate import BRANCH
 from constants.topology import SERVER, SFCC
-from shared.models.topology import Topology
+from shared.models.topology import Link, Topology
 from shared.models.embedding_graph import EmbeddingGraph
 import tensorflow as tf
 import numpy as np
 from utils.tui import TUI
 import pandas as pd
+
 
 class HotCode:
     """
@@ -144,12 +145,19 @@ class Node:
     def __eq__(self, name):
         return self.name == name
 
+
 class EmbedLinks:
     """
     This defines the logic used to embed links.
     """
 
-    def __init__(self, topology: Topology, egs: "list[EmbeddingGraph]", weights: "list[float]", bias: "list[float]")  -> None:
+    def __init__(
+        self,
+        topology: Topology,
+        egs: "list[EmbeddingGraph]",
+        weights: "list[float]",
+        bias: "list[float]",
+    ) -> None:
         """
         Initializes the link embedding.
 
@@ -185,7 +193,11 @@ class EmbedLinks:
             bool: True if the node is a host, False otherwise.
         """
 
-        return node in [host["id"] for host in self._topology["hosts"]] or node == SFCC or node == SERVER
+        return (
+            node in [host["id"] for host in self._topology["hosts"]]
+            or node == SFCC
+            or node == SERVER
+        )
 
     def _constructDF(self) -> pd.DataFrame:
         """
@@ -212,7 +224,10 @@ class EmbedLinks:
             for host in hosts:
                 if f"{switch}_{host}" not in linkIndices:
                     linkIndices.append(f"{switch}_{host}")
-                    links.append(self._hotCode.getNodeCode(switch) + self._hotCode.getNodeCode(host))
+                    links.append(
+                        self._hotCode.getNodeCode(switch)
+                        + self._hotCode.getNodeCode(host)
+                    )
 
         rows: "list[list[int]]" = []
         indices: "list[str]" = []
@@ -226,12 +241,24 @@ class EmbedLinks:
 
         columns: "list[str]" = []
         columns.extend([f"SFC{i}" for i in range(len(self._egs))])
-        columns.extend([f"Source{i}" for i in range(len(self._topology["hosts"]) + len(self._topology["switches"]) + 2)])
-        columns.extend([f"Destination{i}" for i in range(len(self._topology["hosts"]) + len(self._topology["switches"]) + 2)])
-
+        columns.extend(
+            [
+                f"Source{i}"
+                for i in range(
+                    len(self._topology["hosts"]) + len(self._topology["switches"]) + 2
+                )
+            ]
+        )
+        columns.extend(
+            [
+                f"Destination{i}"
+                for i in range(
+                    len(self._topology["hosts"]) + len(self._topology["switches"]) + 2
+                )
+            ]
+        )
 
         return pd.DataFrame(rows, columns=columns, index=indices)
-
 
     def _constructGraph(self) -> nx.Graph:
         """
@@ -247,10 +274,8 @@ class EmbedLinks:
         graph: nx.Graph = nx.Graph()
 
         for link in self._topology["links"]:
-            graph.add_edge(
-                link["source"], link["destination"])
-            graph.add_edge(
-                link["destination"], link["source"])
+            graph.add_edge(link["source"], link["destination"])
+            graph.add_edge(link["destination"], link["source"])
 
         return graph
 
@@ -262,7 +287,9 @@ class EmbedLinks:
             None
         """
 
-        nodeLength: int = len(self._topology["hosts"]) + len(self._topology["switches"]) + 2
+        nodeLength: int = (
+            len(self._topology["hosts"]) + len(self._topology["switches"]) + 2
+        )
         sfcLength: int = len(self._egs)
 
         self._hotCode.addNode(SFCC, nodeLength)
@@ -288,10 +315,12 @@ class EmbedLinks:
         data: pd.DataFrame = self._constructDF()
         layers: "list[int]" = [len(data.columns), 1]
 
-        model = tf.keras.Sequential([
-            tf.keras.Input(shape=(layers[0], )),
-            tf.keras.layers.Dense(layers[1], activation="relu"),
-        ])
+        model = tf.keras.Sequential(
+            [
+                tf.keras.Input(shape=(layers[0],)),
+                tf.keras.layers.Dense(layers[1], activation="relu"),
+            ]
+        )
 
         index: int = 0
         wStartIndex: int = 0
@@ -300,10 +329,16 @@ class EmbedLinks:
         bEndIndex: int = layers[index + 1]
         for layer in model.layers:
             if isinstance(layer, tf.keras.layers.Dense):
-                layer.set_weights([
-                    np.array(self._weights[wStartIndex:wEndIndex]).reshape(layers[index], layers[index + 1]),
-                    np.array(self._bias[bStartIndex:bEndIndex]).reshape(layers[index + 1])
-                ])
+                layer.set_weights(
+                    [
+                        np.array(self._weights[wStartIndex:wEndIndex]).reshape(
+                            layers[index], layers[index + 1]
+                        ),
+                        np.array(self._bias[bStartIndex:bEndIndex]).reshape(
+                            layers[index + 1]
+                        ),
+                    ]
+                )
                 index += 1
                 if index < len(layers) - 1:
                     wStartIndex = wEndIndex
@@ -372,17 +407,24 @@ class EmbedLinks:
                         node.hCost = 0
                     else:
                         node.hCost = self._getHeuristicCost(
-                            sfcID,
-                            neighbor,
-                            destination)
+                            sfcID, neighbor, destination
+                        )
                     node.parent = currentNode
                     node.totalCost = currentNode.totalCost + self._getHeuristicCost(
-                        sfcID,
-                        currentNode.name,
-                        neighbor
+                        sfcID, currentNode.name, neighbor
                     )
 
-                    if len([closedSetNode for closedSetNode in closedSet if closedSetNode.name == neighbor and node.totalCost >= closedSetNode.totalCost]) == 0:
+                    if (
+                        len(
+                            [
+                                closedSetNode
+                                for closedSetNode in closedSet
+                                if closedSetNode.name == neighbor
+                                and node.totalCost >= closedSetNode.totalCost
+                            ]
+                        )
+                        == 0
+                    ):
                         heapq.heappush(openSet, node)
 
             index += 1
@@ -460,52 +502,97 @@ class EmbedLinks:
             list[EmbeddingGraph]: the EGs.
         """
 
+        paths: "dict[str, list[str]]" = {}
+
         for eg in self._egs:
             if "links" not in eg:
                 eg["links"] = []
 
-            paths: "list[str]" = []
             sfcNodes, sfcDivisors = self.parseNodes(nodes[eg["sfcID"]])
 
             for nodeList, divisor in zip(sfcNodes, sfcDivisors):
                 for i in range(len(nodeList) - 1):
-                    try:
-                        if nodeList[i] == nodeList[i + 1]:
-                            continue
-
-                        path = self._findPath(eg["sfcID"], nodeList[i], nodeList[i + 1])
-
-                        for p in range(len(path) - 1):
-                            if f"{path[p]}-{path[p + 1]}" in self._linkData:
-                                if eg["sfcID"] in self._linkData[f"{path[p]}-{path[p + 1]}"]:
-                                    self._linkData[f"{path[p]}-{path[p + 1]}"][eg["sfcID"]] += 1/divisor
-                                else:
-                                    self._linkData[f"{path[p]}-{path[p + 1]}"][eg["sfcID"]] = 1/divisor
-                            elif f"{path[p + 1]}-{path[p]}" in self._linkData:
-                                if eg["sfcID"] in self._linkData[f"{path[p + 1]}-{path[p]}"]:
-                                    self._linkData[f"{path[p + 1]}-{path[p]}"][eg["sfcID"]] += 1/divisor
-                                else:
-                                    self._linkData[f"{path[p + 1]}-{path[p]}"][eg["sfcID"]] = 1/divisor
-                                self._linkData[f"{path[p + 1]}-{path[p]}"][eg["sfcID"]] += 1/divisor
-                            else:
-                                self._linkData[f"{path[p]}-{path[p + 1]}"] = {
-                                    eg["sfcID"]: 1/divisor
-                                }
-
-                        if f"{nodeList[i]}-{nodeList[i + 1]}" in paths:
-                            continue
-
-                        paths.append(f"{nodeList[i]}-{nodeList[i + 1]}")
-
-                    except Exception as e:
-                        TUI.appendToSolverLog(f"Error: {e}", True)
+                    if nodeList[i] == nodeList[i + 1]:
                         continue
 
-                    eg["links"].append({
-                        "source": {"id": path[0]},
-                        "destination": {"id": path[-1]},
-                        "links": path[1:-1],
-                        "divisor": divisor
-                    })
+                    srcDst: str = f"{nodeList[i]}-{nodeList[i + 1]}"
+                    dstSrc: str = f"{nodeList[i + 1]}-{nodeList[i]}"
+
+                    if srcDst not in paths and dstSrc not in paths:
+                        try:
+                            path = self._findPath(
+                                eg["sfcID"], nodeList[i], nodeList[i + 1]
+                            )
+                            paths[srcDst] = path
+                        except Exception as e:
+                            TUI.appendToSolverLog(f"Error: {e}", True)
+                            continue
+
+                        eg["links"].append(
+                            {
+                                "source": {"id": path[0]},
+                                "destination": {"id": path[-1]},
+                                "links": path[1:-1],
+                            }
+                        )
+
+                    path = paths[srcDst] if srcDst in paths else paths[dstSrc]
+
+                    for p in range(len(path) - 1):
+                        link: Link = [
+                            topoLink
+                            for topoLink in self._topology["links"]
+                            if (
+                                topoLink["source"] == path.nodes[p]
+                                and topoLink["destination"] == path.nodes[p + 1]
+                            )
+                            or (
+                                topoLink["source"] == path.nodes[p + 1]
+                                and topoLink["destination"] == path.nodes[p]
+                            )
+                        ][0]
+                        linkDelay: float = (
+                            (link["delay"] / divisor)
+                            if "delay" in link and link["delay"] is not None
+                            else 0
+                        )
+                        if f"{path[p]}-{path[p + 1]}" in self._linkData:
+                            if (
+                                eg["sfcID"]
+                                in self._linkData[f"{path[p]}-{path[p + 1]}"]
+                            ):
+                                pathData = tuple[float, float] = self._linkData[
+                                    f"{path[p]}-{path[p + 1]}"
+                                ][eg["sfcID"]]
+                                divisors: float = pathData[0] + 1 / divisor
+                                delay: float = pathData[1] + linkDelay
+                                self._linkData[f"{path[p]}-{path[p + 1]}"][
+                                    eg["sfcID"]
+                                ] = (divisors, delay)
+                            else:
+                                self._linkData[f"{path[p]}-{path[p + 1]}"][
+                                    eg["sfcID"]
+                                ] = (1 / divisor, linkDelay)
+                        elif f"{path[p + 1]}-{path[p]}" in self._linkData:
+                            if (
+                                eg["sfcID"]
+                                in self._linkData[f"{path[p + 1]}-{path[p]}"]
+                            ):
+                                pathData: tuple[float, float] = self._linkData[
+                                    f"{path[p + 1]}-{path[p]}"
+                                ][eg["sfcID"]]
+                                divisors: float = pathData[0] + 1 / divisor
+                                delay: float = pathData[1] + linkDelay
+                                self._linkData[f"{path[p + 1]}-{path[p]}"][
+                                    eg["sfcID"]
+                                ] = (divisors, delay)
+                            else:
+                                self._linkData[f"{path[p + 1]}-{path[p]}"][
+                                    eg["sfcID"]
+                                ] = (1 / divisor, linkDelay)
+                        else:
+                            self._linkData[f"{path[p]}-{path[p + 1]}"] = {
+                                eg["sfcID"]: (1 / divisor, linkDelay)
+                            }
 
         return self._egs

src/algorithms/surrogacy/utils/scorer.py
@@ -6,7 +6,7 @@ from typing import Tuple
 import numpy as np
 import polars as pl
 from shared.models.embedding_graph import VNF, EmbeddingGraph
-from shared.models.topology import Host, Topology
+from shared.models.topology import Host, Link, Topology
 from algorithms.models.embedding import EmbeddingData, LinkData
 from algorithms.surrogacy.models.traffic import TimeSFCRequests
 from algorithms.surrogacy.utils.demand_predictions import DemandPredictions
@@ -66,10 +66,9 @@ class Scorer:
                 hostResourceData[host]["cpu"],
                 hostCPU if hostCPU is not None else serverCPU,
             )
-            hostResourceData[host]["memory"] = (
-                hostResourceData[host]["memory"] / hostMemory
-                if hostMemory is not None
-                else serverMemory
+            hostResourceData[host]["memory"] = Scorer._getScore(
+                hostResourceData[host]["memory"],
+                hostMemory if hostMemory is not None else serverMemory
             )
 
         return hostResourceData
@@ -79,7 +78,7 @@ class Scorer:
         data: dict[str, float],
         topology: Topology,
         egs: "list[EmbeddingGraph]",
-        linkData: "dict[str, dict[str, float]]",
+        linkData: "dict[str, dict[str, tuple[float, float, int ]]]",
     ) -> "dict[str, float]":
         """
         Gets the link scores.
@@ -91,17 +90,19 @@ class Scorer:
             linkData (dict[str, dict[str, float]]): the link data.
 
         Returns:
-            dict[str, float]: the link scores.
+            dict[str, tuple[float, float, int ]]: the link scores.
         """
 
-        linkScores: "dict[str, float]" = {}
+        linkScoresData: "dict[str, tuple[float, float, int ]]" = {}
         for eg in egs:
             totalLinkScore: float = 0.0
+            linkScores: list[float] = []
+            totalDelay: int = 0
+            checkedLinks: set[str] = set()
             for egLink in eg["links"]:
                 links: "list[str]" = [egLink["source"]["id"]]
                 links.extend(egLink["links"])
                 links.append(egLink["destination"]["id"])
-                reqps: float = data[eg["sfcID"]] if eg["sfcID"] in data else 0
 
                 for linkIndex in range(len(links) - 1):
                     source: str = links[linkIndex]
@@ -109,33 +110,52 @@ class Scorer:
 
                     totalRequests: int = 0
 
-                    if f"{source}-{destination}" in linkData:
-                        for _key, factor in linkData[f"{source}-{destination}"].items():
-                            totalRequests += factor * reqps
-                    elif f"{destination}-{source}" in linkData:
-                        for _key, factor in linkData[f"{destination}-{source}"].items():
-                            totalRequests += factor * reqps
-
-                    bandwidth: float = [
-                        link["bandwidth"]
-                        for link in topology["links"]
+                    link: Link = [
+                        topoLink
+                        for topoLink in topology["links"]
                         if (
-                            link["source"] == source
-                            and link["destination"] == destination
+                            topoLink["source"] == source
+                            and topoLink["destination"] == destination
                         )
                         or (
-                            link["source"] == destination
-                            and link["destination"] == source
+                            topoLink["source"] == destination
+                            and topoLink["destination"] == source
                         )
                     ][0]
 
+                    if f"{source}-{destination}" in linkData:
+                        if f"{source}-{destination}" in checkedLinks:
+                            continue
+                        checkedLinks.add(f"{source}-{destination}")
+                        for key, pathData in linkData[f"{source}-{destination}"].items():
+                            reqps: float = (
+                                data[key] if key in data else 0
+                            )
+                            totalRequests += pathData[0] * reqps
+
+                            if key == eg["sfcID"]:
+                                totalDelay += pathData[1]
+                    elif f"{destination}-{source}" in linkData:
+                        if f"{destination}-{source}" in checkedLinks:
+                            continue
+                        checkedLinks.add(f"{destination}-{source}")
+                        for key, pathData in linkData[f"{destination}-{source}"].items():
+                            reqps: float = (
+                                data[key] if key in data else 0
+                            )
+                            totalRequests += pathData[0] * reqps
+
+                            if key == eg["sfcID"]:
+                                totalDelay += pathData[1]
+
+                    bandwidth: float = link["bandwidth"] if "bandwidth" in link else 1.0
                     linkScore: float = Scorer._getLinkScore(totalRequests, bandwidth)
-
                     totalLinkScore += linkScore
+                    linkScores.append(linkScore)
 
-            linkScores[eg["sfcID"]] = totalLinkScore
+            linkScoresData[eg["sfcID"]] = (totalLinkScore, max(linkScores), (2 * totalDelay))
 
-        return linkScores
+        return linkScoresData
 
     @staticmethod
     def getHostLinkScoresForEachSFC(
@@ -183,8 +203,15 @@ class Scorer:
         maxMemory: float = (
             max([host["memory"] for host in hosts.values()]) if len(hosts) > 0 else 0.0
         )
-        link: float = (
-            linkScores[sfc] if linkScores is not None and sfc in linkScores else 0.0
+
+        totalLinkScore: float = (
+            linkScores[sfc][0] if linkScores is not None and sfc in linkScores else 0.0
+        )
+        maxLinkScore: float = (
+            linkScores[sfc][1] if linkScores is not None and sfc in linkScores else 0.0
+        )
+        totalDelay: int = (
+            linkScores[sfc][2] if linkScores is not None and sfc in linkScores else 0
         )
 
         dt = np.dtype(
@@ -194,7 +221,9 @@ class Scorer:
                 ("reqps", np.float64),
                 ("max_cpu", np.float64),
                 ("max_memory", np.float64),
-                ("link", np.float64),
+                ("total_link_score", np.float64),
+                ("max_link_score", np.float64),
+                ("total_delay", np.int32),
             ]
         )
         newData: np.array = np.array(
@@ -205,7 +234,9 @@ class Scorer:
                     float(sfcReqps),
                     float(maxCPU),
                     float(maxMemory),
-                    float(link),
+                    float(totalLinkScore),
+                    float(maxLinkScore),
+                    int(totalDelay)
                 )
             ],
             dtype=dt,
@@ -237,10 +268,9 @@ class Scorer:
         hostScores: "dict[str, ResourceDemand]" = Scorer.getHostScores(
             timeData, topology, embeddingData, demandPredictor
         )
-        linkScores: "dict[str, float]" = Scorer.getLinkScores(
+        linkScores: "dict[str, tuple[float, float, int ]]" = Scorer.getLinkScores(
             timeData, topology, egs, linkData
         )
-
         hostLinkScores: np.array = None
         for key, value in timeData.items():
             hostLinkScore: np.array = Scorer.getHostLinkScoresForEachSFC(
@@ -277,7 +307,6 @@ class Scorer:
             np.array: the SFC scores (time, SFC ID, reqps, max CPU, max memory, link score).
         """
 
-        timeDataList: "list[pl.DataFrame]" = []
         outputData: np.array = None
         for time, timeData in enumerate(data):
             timeSlotData: np.array = Scorer.getHostLinkScoresForEachTimeSlot(