08d6970c82ce067702cb081dcdc53081fd9ecd20
Thivi <theviyanthan@outlook.com>
2026-06-17 12:39:32 +0100

    feat(algorithms): update root IP address

--- src/algorithms/hybrid/constants/root_evolver.py
+++ src/algorithms/hybrid/constants/root_evolver.py
@@ -5,4 +5,4 @@ This file defines the constants used in the root evolver service/client of the H
 ROOT_PATH: str = "/root"
 SELECT_NEIGHBOUR_PATH: str = "/root-neighbour"
 SERVICE_PORT: int = 3333
-SERVICE_ADDRESS: str = "http://localhost"
+SERVICE_ADDRESS: str = "http://192.168.1.1"
