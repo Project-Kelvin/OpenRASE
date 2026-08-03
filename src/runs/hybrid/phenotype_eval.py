619948fddddfaea8934aec7b1db3ed01eb0b7178
Thivi <theviyanthan@outlook.com>
2026-08-03 14:20:14 +0100

    feat(runs): add experiment to evaluate phenotype space

---
+++ src/runs/hybrid/phenotype_eval.py
@@ -0,0 +1,149 @@
+"""
+This defines the function to evaluate GENESIS'S ability to explore the phenotype space.
+"""
+
+from typing import cast
+
+import numpy as np
+from shared.models.embedding_graph import VNF, EmbeddingGraph
+from shared.models.sfc_request import SFCRequest
+from shared.models.topology import Topology
+
+from algorithms.hybrid.models.individuals import GenesisIndividual, Individual
+from algorithms.hybrid.utils.extract_weights import generateRandomWeight
+from algorithms.hybrid.utils.genesis import GenesisUtils
+from algorithms.models.embedding import DecodedIndividual
+from constants.topology import SERVER, SFCC
+from utils.embedding_graph import traverseVNF
+
+
+def run() -> None:
+    """
+    This function runs the GENESIS algorithm on the phenotype space and evaluates its performance.
+    """
+
+    topology: Topology = cast(Topology, {
+        "hosts": [
+            {
+                "id": "h1"
+            },
+            {
+                "id": "h2"
+            }
+        ],
+        "switches": [
+            {
+                "id": "s1"
+            },
+            {
+                "id": "s2"
+            },
+            {
+                "id": "s3"
+            },
+            {
+                "id": "s4"
+            }
+        ],
+        "links": [
+            {
+                "source": SFCC,
+                "destination": "s1",
+                "bandwidth": 1000,
+                "delay": 10
+            },
+            {
+                "source": "h2",
+                "destination": "s4",
+                "bandwidth": 1000,
+                "delay": 10
+            },
+            {
+                "source": "h1",
+                "destination": "s2",
+                "bandwidth": 1000,
+                "delay": 10
+            },
+            {
+                "source": SERVER,
+                "destination": "s3",
+                "bandwidth": 1000,
+                "delay": 10
+            },
+            {
+                "source": "s1",
+                "destination": "s2",
+                "bandwidth": 1000,
+                "delay": 10
+            },
+            {
+                "source": "s2",
+                "destination": "s3",
+                "bandwidth": 1000,
+                "delay": 10
+            },
+            {
+                "source": "s3",
+                "destination": "s4",
+                "bandwidth": 1000,
+                "delay": 10
+            }
+        ]
+    })
+
+    sfcrs: list[SFCRequest] = [
+        cast(SFCRequest, {
+            "sfcrID": "sfcr1",
+            "vnfs": ["waf", "tm"]
+        })
+    ]
+
+    ccOrder: list[list[str]] = [["waf", "tm"], ["tm", "waf"]]
+    hostOrder: list[list[str]] = [["h1", "h2"], ["h2", "h1"]]
+    linksOrder: dict[str, list[list[str]]] = {
+        "sfcc-h1": [["s1", "s2"], ["s1", "s4", "s3", "s2"]],
+        "sfcc-h2": [["s1", "s4"], ["s1", "s2", "s3", "s4"]],
+        "h1-h2": [["s2", "s3", "s4"], ["s2", "s1", "s4"]],
+        "h2-h1": [["s4", "s3", "s2"], ["s4", "s1", "s2"]],
+        "h1-server": [["s2", "s3"], ["s2", "s1", "s4", "s3"]],
+        "h2-server": [["s4", "s3"], ["s4", "s1", "s2", "s3"]],
+    }
+
+    combinations: list[tuple[list[str], list[str], tuple[list[str], list[str], list[str]]]] = []
+
+    for cc in ccOrder:
+        for host in hostOrder:
+            for sfccLink in linksOrder[f"sfcc-{host[0]}"]:
+                for hostLink in linksOrder[f"{host[0]}-{host[1]}"]:
+                    for serverLink in linksOrder[f"{host[1]}-server"]:
+                        combinations.append((cc, host, (sfccLink, hostLink, serverLink)))
+
+    GenesisUtils.init(sfcrs, topology, 2, 0.05, 1.0, np.pi)
+    individual: Individual = GenesisUtils.generateRandomGenesisIndividual(Individual, topology, sfcrs)
+    decodedIndividual: DecodedIndividual = GenesisUtils.decodeIndividual(cast(GenesisIndividual, individual), 0, topology, sfcrs)
+    eg: EmbeddingGraph = decodedIndividual[1][0]
+
+    egCcOrder: list[str] = []
+    egHostOrder: list[str] = []
+    egLinksOrder: dict[str, list[str]] = {}
+    def parseVNF(vnf: VNF, _depth: int) -> None:
+        """
+        Parses a VNF and its children recursively, printing their details.
+
+        Parameters:
+            vnf (VNF): The VNF to parse.
+            _depth (int): The current depth in the hierarchy for indentation.
+
+        Returns:
+            None
+        """
+
+        vnfId: str = vnf["vnfID"]
+        egCcOrder.append(vnfId)
+        hostId: str = vnf["hostID"]
+        egHostOrder.append(hostId)
+
+    traverseVNF(eg["vnfs"], parseVNF)
+
+    for link in eg["links"]:
+        egLinksOrder[f"{link['source']['id']}-{link['destination']['id']}"] = link["links"]
